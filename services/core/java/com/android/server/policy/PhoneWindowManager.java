8a21fcc8527708ddc1f7548763dc0b1bf7ae7f48
chunshu liu <chunshu.x.liu@sonymobile.com>
2014-12-26 11:14:20 +0900

    Turn LCD off when mirroring display to save power
    registered as service, getSystemsService() then call it to turn lcd off
    
    Test: mirror screen to TV, phone lcd is off
    
    BenzoEdit: Fixed up for Pie
    Change-Id: I690275d126afbf8ab56b608959a32343fba91f12
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -296,6 +296,8 @@ import com.android.server.wm.DisplayFrames;
 import com.android.server.wm.WindowManagerInternal;
 import com.android.server.wm.WindowManagerInternal.AppTransitionListener;
 
+import com.android.internal.mirrorpowersave.LcdPowerSaveInternal;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -508,6 +510,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     // Assigned on main thread, accessed on UI thread
     volatile VrManagerInternal mVrManagerInternal;
+
+    private LcdPowerSaveInternal mLcdPowerSaveInternal;
 
     // Vibrator pattern for haptic feedback of a long press.
     long[] mLongPressVibePattern;
@@ -2444,6 +2448,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             startedGoingToSleep(WindowManagerPolicy.OFF_BECAUSE_OF_USER);
             finishedGoingToSleep(WindowManagerPolicy.OFF_BECAUSE_OF_USER);
         }
+
+        mLcdPowerSaveInternal = LocalServices.getService(LcdPowerSaveInternal.class);
 
         mWindowManagerInternal.registerAppTransitionListener(
                 mStatusBarController.getAppTransitionListener());
@@ -6317,6 +6323,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     public int interceptKeyBeforeQueueing(KeyEvent event, int policyFlags) {
         if (!mSystemBooted) {
             // If we have not yet booted, don't let key events do anything.
+            return 0;
+        }
+
+        if (mLcdPowerSaveInternal.interceptPowerKeyBeforeQueueingWhenLcdOff(event, policyFlags)) {
             return 0;
         }
 
