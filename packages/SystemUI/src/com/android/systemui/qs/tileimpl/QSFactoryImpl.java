7246d9fd290db67a2b79c64a21740c04e7a2ced1
Alex Cruz <mazdarider23@gmail.com>
2015-12-22 13:31:32 -0800

    [SQUASHED] QS: Navigation Bar tile
    Change-Id: I082876b26945facbbbc06d1175964267e4c05152
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -41,6 +41,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.HWKeysTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.NavBarTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
@@ -140,6 +141,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new RebootTile(mHost);
             case "music":
                 return new MusicTile(mHost);
+            case "navbar":
+                return new NavBarTile(mHost);
         }
 
         // Intent tiles.
