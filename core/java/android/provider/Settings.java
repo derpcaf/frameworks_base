a1794774f213f502ee8d9ec94d2bd5ca24fffbee
Nico60 <ndhouailly@gmail.com>
2016-05-16 21:48:30 +0200

    Add animations to quick settings tiles [1/2]
    @mydongistiny: Brought from nougat->8.1
    Change-Id: I81e79cfccfc4565b1f7a6292b5f0b88be753df74
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5007,6 +5007,20 @@ public final class Settings {
         public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
 
         /**
+         * Change quick settings tiles animation style
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_STYLE = "anim_tile_style";
+
+        /**
+         * Change quick settings tiles animation duration
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_DURATION = "anim_tile_duration";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
