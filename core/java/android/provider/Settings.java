c298d43b00f80081238e2356e8934d6c2a8f977e
Alex Naidis <alex.naidis@linux.com>
2017-09-12 14:04:27 +0800

    Immersive Recents [1/2] Name says it all. Made this when someone had asked about how recents look with Touchwiz, simple enough.
    May expand this to other shit but for now, it do what it do hahaha
    
    0 - Default AOSP
    1 - Fullscreen
    2 - Statusbar only
    3 - NavBar only

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5631,6 +5631,16 @@ public final class Settings {
         /** @hide */
         private static final Validator DEVICE_FEATURE_SETTINGS_VALIDATOR = ANY_STRING_VALIDATOR;
 
+	/**
+         * Immersive recents options
+         *
+         * 0 = Default AOSP look
+         * 1 = Statusbar only
+         * 2 = Navbar only
+         * 3 = Fullscreen
+         */
+        public static final String IMMERSIVE_RECENTS = "immersive_recents";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
