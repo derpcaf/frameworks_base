3d58d1445eb7c779ce3b53b42d69ce230c1ea89d
Shubham Singh <coolsks94@gmail.com>
2018-10-08 17:09:12 +0530

    fwb: Allow users to switch between Pie/Oreo recents [1/3]
    I personally don't like Pie's recent style built in launcher3,
    Hence allow to switch between recents components(Quickstep/SystemUI) without much hassle.
    Other two parts of commit in Extensions and Settings will also take care of swipe up gesture.
    If it is enabled it will be disabled and removed from Settings>System>Gestures, if in case Oreo style recents is selected by user.
    
    Commit's logic is based on https://github.com/PotatoProject/frameworks_base/commit/74f8b464a944cff9f382148521aab52d0c054136 , so huge thanks to @deletescape
    
    Change-Id: I2c0657bb798c515bd946bf1885d355520a8b98c0
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5339,6 +5339,18 @@ public final class Settings {
         public static final String DOT_COLOR_SWITCH = "dot_color_switch";
 
         /**
+         * Which component to use for Recents UI
+         * 0 - Pie Recents (Quickstep)
+         * 1 - Oreo Recents (SystemUI)
+         * @hide
+         */
+        public static final String RECENTS_COMPONENT = "recents_component";
+
+        /** @hide */
+        public static final Validator RECENTS_COMPONENT_VALIDATOR =
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 1);
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -5417,6 +5429,7 @@ public final class Settings {
             OMNI_LONG_PRESS_POWER_TORCH,
             NOTIFICATION_SOUND_VIB_SCREEN_ON,
             ONE_HAND_MODE_ENABLED,
+            RECENTS_COMPONENT,
         };
 
         /**
@@ -5564,6 +5577,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_COLUMNS_LANDSCAPE);
             PRIVATE_SETTINGS.add(QS_HEADER_STYLE);
             PRIVATE_SETTINGS.add(ONE_HAND_MODE_ENABLED);
+            PRIVATE_SETTINGS.add(RECENTS_COMPONENT);
         }
 
 
@@ -5682,6 +5696,7 @@ public final class Settings {
             VALIDATORS.put(MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD_VALIDATOR);
             VALIDATORS.put(NOTIFICATION_SOUND_VIB_SCREEN_ON, NOTIFICATION_SOUND_VIB_SCREEN_ON_VALIDATOR);
             VALIDATORS.put(ONE_HAND_MODE_ENABLED, ONE_HAND_MODE_ENABLED_VALIDATOR);
+            VALIDATORS.put(RECENTS_COMPONENT,RECENTS_COMPONENT_VALIDATOR);
         }
 
         /**
