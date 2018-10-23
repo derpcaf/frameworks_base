0816340c12478b2046d1910585f728c416038b22
SuperDroidBond <superdroidbond@yahoo.com>
2018-10-23 13:47:20 +0530

    [SQUASH] Revert All QS Tile Columns & Row Configs
    Rever "base: SystemUI: make QS columns count configurable"
    
    This reverts commit 50ccc7912e48a4c85633ccb50de8081065870765.
    
    Revert "base: SystemUI: qs quickbar columns config"
    
    This reverts commit 0c6a95c9e4654baa2ec4172055c952d4bf62cf5f.
    
    Revert "base: SystemUI: add seven and eight qs columns for portrait"
    
    This reverts commit 5bbb978e38e2e59d50ea04837c4488fadc787ace.
    
    Revert "base: make qs lanscape rows count configurable"
    
    This reverts commit 6d6b2b257aa2694d6af08a99313e9ae1aff6ae01.
    
    Revert "base: SystemUI: add QS rows config"
    
    This reverts commit 1f33536a7c742d8cd251bd64b6b051aa2a95df90.
    
    Revert "base: SystemUI: add back QS hide label"
    
    This reverts commit fa5ed4f45fbb5a344978006f0bf28ae1e9fb3eee.
    
    Revert "base: SystemUI: make QS columns count configurable"
    
    This reverts commit fc2829e7242e3de00143c13ce3ba4502d6d789a0.

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -100,11 +100,6 @@ public abstract class QSTileImpl<TState extends State> implements QSTile {
 
     protected Vibrator mVibrator;
 
-    @Override
-    public boolean isDualTarget() {
-        return false;
-    }
-
     /**
      * Declare the category of this tile.
      *
@@ -282,10 +277,8 @@ public abstract class QSTileImpl<TState extends State> implements QSTile {
     }
 
     protected void handleLongClick() {
-        if (getLongClickIntent() != null) {
-            Dependency.get(ActivityStarter.class).postStartActivityDismissingKeyguard(
-                    getLongClickIntent(), 0);
-        }
+        Dependency.get(ActivityStarter.class).postStartActivityDismissingKeyguard(
+                getLongClickIntent(), 0);
     }
 
     public abstract Intent getLongClickIntent();
