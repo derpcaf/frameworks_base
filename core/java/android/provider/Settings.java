706982b0e91e4a23e3e4b27fd4314c5fdcc2efc7
SuperDroidBond <superdroidbond@yahoo.com>
2018-10-25 00:43:29 +0530

    LS OwnerInfo Fonts & Size Options [1/2] SuperdroidBond:- It's done on the basis of LS Date Font & Size.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5404,7 +5404,6 @@ public final class Settings {
         public static final String LOCKSCREEN_OWNER_INFO_COLOR = "lockscreen_owner_info_color";
 
          /**
-         * Change the color of the lockscreen bottom camera shortcut
          *
          * @hide
          */
@@ -5448,6 +5447,20 @@ public final class Settings {
          * @hide
          */
         public static final String LOCK_SCREEN_WEATHER_ICON_COLOR = "lock_screen_weather_icon_color";
+
+	/**
+	 ** Change fonts for the system lockscreen Owner info
+	 **
+	 ** @hide
+	 **/
+        public static final String LOCK_OWNERINFO_FONTS = "lock_ownerinfo_fonts";
+
+        /**
+	 ** Change font size for the system lockscreen owner
+	 **
+	 ** @hide
+	 **/
+        public static final String LOCKOWNER_FONT_SIZE= "lockowner_font_size";
 
         /**
          * 0 - wallpaper based
