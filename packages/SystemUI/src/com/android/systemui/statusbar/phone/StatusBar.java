91392e1f4fe52a48dcfca596be6548d10f07fee0
Alex Cruz <du.alexcruz@gmail.com>
2018-12-31 03:36:06 -0600

    Introduce lockscreen clock/date styles [1/2]
    Inspired by OnePlus and their recent options to switch lockscreen
    clock styles on the OP6, I took it upon myself to do the same with our ROM.
    
    This isn't as fancy as OnePlus' because is just using a list preference instead
    of adding in all the eye candy but it gets the job done!
    
    Below is a run down of what took place...
    
    - Mirrored the analog clock class to avoid issues with any apps that may
    or may not be utilizing. Google deprecated this class back in 2015 but there's
    still a chance that something may go wrong some where. Better safe than sorry.
    
    /core/java/android/widget/AnalogClock.java@o8x
    
    - Added the analog clock to the keyguard and made it optional
    - Manipulated the clockview format for both 12 and 24 hour to achieved the 'sammy' style
    - Made the sammy style optional and created a switch case for all 3.
    - Set padding, singeline and other layout changes programmatically to minimize the jank
    - Reworked/cleaned up some existing code (Huge thanks to @ezio84)
    - Created bold sammy by using html formatting, based off the sammy one
    - Created bold digital by using html formatting, based off the digital one
    - Vectorized the analog clock images so we can theme
    - Modified Analog clock class to fix ambient display issues (@ezio84)
    
    For right now there's five clock styles:
    
    Clock styles---
    - Default
    - Digital (bold)
    - Analog
    - Sammy
    - Sammy (bold)
    
    Change-Id: I57d38ab979a203c0c5b5e6ace17fcb52c5e8fff0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4919,6 +4919,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                      Settings.Secure.PULSE_APPS_BLACKLIST),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_CLOCK_SELECTION),
+                    false, this, UserHandle.USER_ALL);
 	 }
 
         @Override
@@ -4938,6 +4941,9 @@ public class StatusBar extends SystemUI implements DemoMode,
                 updateQSHeaderStyle();
             } else if (uri.equals(Settings.Secure.getUriFor(Settings.Secure.PULSE_APPS_BLACKLIST))) {
                 setPulseBlacklist();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_CLOCK_SELECTION))) {
+                updateKeyguardStatusSettings();
 	    }
 	    update();
         }
@@ -4950,6 +4956,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             updateQsPanelResources();
 	    setPulseBlacklist();
 	 }
+    }
+    private void updateKeyguardStatusSettings() {
+        mNotificationPanel.updateKeyguardStatusSettings();
     }
 
     private void updateQsPanelResources() {
