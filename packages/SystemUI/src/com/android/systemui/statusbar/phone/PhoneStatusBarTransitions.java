74767fb73503af12f07bce4ed6981dfad9b1ecd8
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-02-06 21:58:06 +0100

    Weather: Move statusbar weather to QS statusbar header
    With the notch and limitation of icons in the status bar,there's almost
    no room left for weather and is really a tight fit among others.
    So lets move it to the left side of the clock in QS statusbar header.
    
    Also implemented a method to be invoked on orientation change and
    use colors that adapt based on the scrim/wallpaper behind quick settings.
    
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>
    Change-Id: Icc429f30bbf29d3232864908711105c37865e511

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mBattery, mClock, mNetworkTraffic, mLogoIcon, mLogoIconRight, mCenterClock, mRightClock, mWeatherTextView, mWeatherImageView;
+    private View mLeftSide, mStatusIcons, mBattery, mClock, mNetworkTraffic, mLogoIcon, mLogoIconRight, mCenterClock, mRightClock;
 
     private Animator mCurrentAnimation;
 
@@ -51,8 +51,6 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mCenterClock = mView.findViewById(R.id.center_clock);
         mRightClock = mView.findViewById(R.id.right_clock);
         mNetworkTraffic = mView.findViewById(R.id.networkTraffic);
-        mWeatherTextView = mView.findViewById(R.id.weather_temp);
-        mWeatherImageView = mView.findViewById(R.id.weather_image);
 	mLogoIcon = mView.findViewById(R.id.status_bar_logo);
 	mLogoIconRight = mView.findViewById(R.id.status_bar_logo_right);
         applyModeBackground(-1, getMode(), false /*animate*/);
@@ -113,14 +111,13 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mLeftSide.setAlpha(newAlpha);
             mStatusIcons.setAlpha(newAlpha);
             mBattery.setAlpha(newAlphaBC);
+
             mClock.setAlpha(newAlphaBC);
             mCenterClock.setAlpha(newAlphaBC);
             mRightClock.setAlpha(newAlphaBC);
             mNetworkTraffic.setAlpha(newAlpha);
 	    mLogoIcon.setAlpha(newAlpha);
 	    mLogoIconRight.setAlpha(newAlpha);
-            mWeatherTextView.setAlpha(newAlphaBC);
-            mWeatherImageView.setAlpha(newAlphaBC);
         }
     }
 }
